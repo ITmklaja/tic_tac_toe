d1414e211a334b4d4b36ffbbcd8520f5a3431682
Maciek Klaja <93444048+ITmklaja@users.noreply.github.com>
2022-11-06 11:41:00 +0100

    Add files via upload

---
+++ game.js
@@ -0,0 +1,204 @@
+function newcolor(){
+    var x = document.getElementById("input-color");
+    var currentVal = x.value;
+    document.getElementById('go').style.color = currentVal;
+
+    const border1 = document.querySelectorAll('.brbottom');
+    border1.forEach(brbottom => {
+    brbottom.style.borderBottom = '1px solid' + currentVal;
+    });
+
+    const border2 = document.querySelectorAll('.brright');
+    border2.forEach(brright => {
+    brright.style.borderRight = '1px solid' + currentVal;
+    });
+    
+    document.getElementsByTagNameNS('http://www.w3.org/2000/svg', 'svg').style.fill = currentVal;
+
+    document.querySelector(".svgx").getSVGDocument().getElementsByClassName('.svgx').setAttribute("fill", currentVal)
+
+
+    const svg1 = document.querySelectorAll('.svgx');
+    svg1.forEach(svgx => {
+    svgx.style.fill = currentVal;
+    });
+
+    const svg2 = document.querySelectorAll('.svgcircle');
+    svg3.forEach(svgcircle => {
+    svgcircle.style.fill = currentVal;
+    });
+
+}
+
+let move = 0;
+let values = [];
+let winner = 0;
+for (let i = 0; i < 9; i++){
+    values[i] = 0;
+}
+
+function check(){
+
+    if (move == 9){
+        document.getElementById('p-go').innerHTML = '<section id="who"></section><p id="go"></p>';
+        document.getElementById('go').innerHTML = 'Draw';
+    }
+    
+    for (let i = 0; i <=8; i += 3){
+        if (values[i+0] == 1 && values[i+1] == 1 && values[i+2] == 1){
+            winner = 1;
+        }else if (values[i+0] == 2 && values[i+1] == 2 && values[i+2] == 2){
+            winner = 2;
+        }
+    }
+
+    for (let i = 0; i <=2; i ++){
+        if (values[i+0] == 1 && values[i+3] == 1 && values[i+6] == 1){
+            winner = 1;
+        } else if (values[i+0] == 2 && values[i+3] == 2 && values[i+6] == 2){
+            winner = 2;
+        }
+    }
+
+    if (values[0] == 1 && values[4] == 1 && values[8] == 1 || values[2] == 1 && values[4] == 1 && values[6] == 1){
+        winner = 1;
+    } else if (values[0] == 2 && values[4] == 2 && values[8] == 2 || values[2] == 2 && values[4] == 2 && values[6] == 2){
+        winner = 2;
+    }
+
+    if (winner == 1){
+        document.getElementById('p-go').innerHTML = '<section id="who"></section><p id="go"></p>';
+        document.getElementById('who').innerHTML = '<svg class="svg-go-x" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M310.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L160 210.7 54.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L114.7 256 9.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 301.3 265.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L205.3 256 310.6 150.6z"/></svg>';
+        document.getElementById('go').innerHTML = 'wins';
+    } else if(winner == 2){
+        document.getElementById('p-go').innerHTML = '<section id="who"></section><p id="go"></p>';
+        document.getElementById('who').innerHTML = '<svg class="svg-go-circle" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M512 256C512 397.4 397.4 512 256 512C114.6 512 0 397.4 0 256C0 114.6 114.6 0 256 0C397.4 0 512 114.6 512 256zM256 48C141.1 48 48 141.1 48 256C48 370.9 141.1 464 256 464C370.9 464 464 370.9 464 256C464 141.1 370.9 48 256 48z"/></svg>';
+        document.getElementById('go').innerHTML = 'wins';
+    }
+}
+
+function get1(){
+    if (values [0] == 0 && move < 9 && winner == 0){
+        if(move%2 == 0 ){
+            document.getElementById('s1').innerHTML = '<svg class="svgx" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M310.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L160 210.7 54.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L114.7 256 9.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 301.3 265.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L205.3 256 310.6 150.6z"/></svg>';
+            values[0] = 1;
+        }else{
+            document.getElementById('s1').innerHTML = '<svg class="svgcircle" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M512 256C512 397.4 397.4 512 256 512C114.6 512 0 397.4 0 256C0 114.6 114.6 0 256 0C397.4 0 512 114.6 512 256zM256 48C141.1 48 48 141.1 48 256C48 370.9 141.1 464 256 464C370.9 464 464 370.9 464 256C464 141.1 370.9 48 256 48z"/></svg>';
+            values[0] = 2;
+        }    
+    move ++;
+    check();        
+    }
+}
+
+function get2(){
+    if (values[1] == 0 && move < 9 && winner == 0){
+        if(move%2 == 0 ){
+            document.getElementById('s2').innerHTML = '<svg class="svgx" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M310.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L160 210.7 54.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L114.7 256 9.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 301.3 265.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L205.3 256 310.6 150.6z"/></svg>';
+            values[1] = 1;
+        }else{
+            document.getElementById('s2').innerHTML = '<svg class="svgcircle" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M512 256C512 397.4 397.4 512 256 512C114.6 512 0 397.4 0 256C0 114.6 114.6 0 256 0C397.4 0 512 114.6 512 256zM256 48C141.1 48 48 141.1 48 256C48 370.9 141.1 464 256 464C370.9 464 464 370.9 464 256C464 141.1 370.9 48 256 48z"/></svg>';
+            values[1] = 2;
+        }    
+    move ++;
+    check(); 
+    }
+}
+
+function get3(){
+    if (values[2] == 0 && move < 9 && winner == 0){
+        if(move%2 == 0 ){
+            document.getElementById('s3').innerHTML = '<svg class="svgx" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M310.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L160 210.7 54.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L114.7 256 9.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 301.3 265.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L205.3 256 310.6 150.6z"/></svg>';
+            values[2] = 1;
+        }else{
+            document.getElementById('s3').innerHTML = '<svg class="svgcircle" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M512 256C512 397.4 397.4 512 256 512C114.6 512 0 397.4 0 256C0 114.6 114.6 0 256 0C397.4 0 512 114.6 512 256zM256 48C141.1 48 48 141.1 48 256C48 370.9 141.1 464 256 464C370.9 464 464 370.9 464 256C464 141.1 370.9 48 256 48z"/></svg>';
+            values[2] = 2;
+        }   
+    move ++;
+    check(); 
+    }
+}
+
+function get4(){
+    if (values[3] == 0 && move < 9 && winner == 0){
+        if(move%2 == 0 ){
+            document.getElementById('s4').innerHTML = '<svg class="svgx" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M310.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L160 210.7 54.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L114.7 256 9.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 301.3 265.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L205.3 256 310.6 150.6z"/></svg>';
+            values[3] = 1;
+        }else{
+            document.getElementById('s4').innerHTML = '<svg class="svgcircle" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M512 256C512 397.4 397.4 512 256 512C114.6 512 0 397.4 0 256C0 114.6 114.6 0 256 0C397.4 0 512 114.6 512 256zM256 48C141.1 48 48 141.1 48 256C48 370.9 141.1 464 256 464C370.9 464 464 370.9 464 256C464 141.1 370.9 48 256 48z"/></svg>';
+            values[3] = 2;
+        }
+    move ++;
+    check();       
+    }
+}
+
+function get5(){
+    if (values[4] == 0 && move < 9 && winner == 0){
+        if(move%2 == 0 ){
+            document.getElementById('s5').innerHTML = '<svg class="svgx" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M310.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L160 210.7 54.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L114.7 256 9.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 301.3 265.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L205.3 256 310.6 150.6z"/></svg>';
+            values[4] = 1;
+        }else{
+            document.getElementById('s5').innerHTML = '<svg class="svgcircle" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M512 256C512 397.4 397.4 512 256 512C114.6 512 0 397.4 0 256C0 114.6 114.6 0 256 0C397.4 0 512 114.6 512 256zM256 48C141.1 48 48 141.1 48 256C48 370.9 141.1 464 256 464C370.9 464 464 370.9 464 256C464 141.1 370.9 48 256 48z"/></svg>';
+            values[4] = 2;
+        }    
+    move ++;
+    check(); 
+    }
+}
+
+function get6(){
+    if (values[5] == 0 && move < 9 && winner == 0){
+        if(move%2 == 0 ){
+            document.getElementById('s6').innerHTML = '<svg class="svgx" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M310.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L160 210.7 54.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L114.7 256 9.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 301.3 265.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L205.3 256 310.6 150.6z"/></svg>';
+            values[5] = 1;
+        }else{
+            document.getElementById('s6').innerHTML = '<svg class="svgcircle" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M512 256C512 397.4 397.4 512 256 512C114.6 512 0 397.4 0 256C0 114.6 114.6 0 256 0C397.4 0 512 114.6 512 256zM256 48C141.1 48 48 141.1 48 256C48 370.9 141.1 464 256 464C370.9 464 464 370.9 464 256C464 141.1 370.9 48 256 48z"/></svg>';
+            values[5] = 2;
+        }    
+    move ++;
+    check(); 
+    }
+}
+
+function get7(){
+    if (values[6] == 0 && move < 9 && winner == 0){
+        if(move%2 == 0 ){
+            document.getElementById('s7').innerHTML = '<svg class="svgx" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M310.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L160 210.7 54.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L114.7 256 9.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 301.3 265.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L205.3 256 310.6 150.6z"/></svg>';
+            values[6] = 1;
+        }else{
+            document.getElementById('s7').innerHTML = '<svg class="svgcircle" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M512 256C512 397.4 397.4 512 256 512C114.6 512 0 397.4 0 256C0 114.6 114.6 0 256 0C397.4 0 512 114.6 512 256zM256 48C141.1 48 48 141.1 48 256C48 370.9 141.1 464 256 464C370.9 464 464 370.9 464 256C464 141.1 370.9 48 256 48z"/></svg>';
+            values[6] = 2;
+        }    
+    move ++;
+    check(); 
+    }
+}
+
+function get8(){
+    if (values[7] == 0 && move < 9 && winner == 0){
+        if(move%2 == 0 ){
+            document.getElementById('s8').innerHTML = '<svg class="svgx" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M310.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L160 210.7 54.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L114.7 256 9.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 301.3 265.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L205.3 256 310.6 150.6z"/></svg>';
+            values[7] = 1;
+        }else{
+            document.getElementById('s8').innerHTML = '<svg class="svgcircle" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M512 256C512 397.4 397.4 512 256 512C114.6 512 0 397.4 0 256C0 114.6 114.6 0 256 0C397.4 0 512 114.6 512 256zM256 48C141.1 48 48 141.1 48 256C48 370.9 141.1 464 256 464C370.9 464 464 370.9 464 256C464 141.1 370.9 48 256 48z"/></svg>';
+            values[7] = 2;
+        }
+    move ++;
+    check(); 
+    }
+}
+
+function get9(){
+    if (values[8] == 0 && move < 9 && winner == 0){
+        if(move%2 == 0 ){
+            document.getElementById('s9').innerHTML = '<svg class="svgx" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M310.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L160 210.7 54.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L114.7 256 9.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 301.3 265.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L205.3 256 310.6 150.6z"/></svg>';
+            values[8] = 1;
+        }else{
+            document.getElementById('s9').innerHTML = '<svg class="svgcircle" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M512 256C512 397.4 397.4 512 256 512C114.6 512 0 397.4 0 256C0 114.6 114.6 0 256 0C397.4 0 512 114.6 512 256zM256 48C141.1 48 48 141.1 48 256C48 370.9 141.1 464 256 464C370.9 464 464 370.9 464 256C464 141.1 370.9 48 256 48z"/></svg>';
+            values[8] = 2;
+        }
+    move ++;
+    check(); 
+    }
+}
